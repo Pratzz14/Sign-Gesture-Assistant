04772dc34cc6548fd1cba0e11f59716271cb9395
Pratik Mahankal <pratik.mahankal14@gmail.com>
2021-05-12 20:45:05 +0530

    Hand Tracking module using MediaPipe API

---
+++ HandTrackingModule.py
@@ -0,0 +1,101 @@
+'''
+This modeule gives the mapped location given by the mediapipe API.
+
+'''
+import cv2
+import mediapipe as mp
+import time
+
+
+class handDetector():
+    '''
+    Two functions:
+        findHands:
+            Finds the joint points using MediaPipe API.
+            If draw=True : draws the line with joints.
+
+        findPosition:
+            Position of the joints.
+
+    '''
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        '''
+        Args:
+        img : The Output of the OpenCV image.
+        draw :  Points mapped to joins and lines conneting to the points.
+                If  True : then draw
+                    False : do not draw
+
+        '''
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        '''
+        Args:
+        img : The Output of the OpenCV image.
+        draw :  Points mapped to joins and lines conneting to the points.
+                If  True : then draw
+                    False : do not draw
+        handNo : Number of hands detected.
+        '''
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        return lmList
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(1)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
